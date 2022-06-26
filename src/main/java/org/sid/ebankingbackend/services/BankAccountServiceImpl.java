44a80235f74c7468a3d1dba233a729ad7970e8dd
oualid el garrai <walidelgarrai@gmail.com>
2022-06-26 18:39:13 +0200

    C4

--- src/main/java/org/sid/ebankingbackend/services/BankAccountServiceImpl.java
+++ src/main/java/org/sid/ebankingbackend/services/BankAccountServiceImpl.java
@@ -191,5 +191,12 @@ public class BankAccountServiceImpl implements BankAccountService{
         return accountHistoryDTO;
     }
 
+    @Override
+    public List<CustomerDTO> searchCustomers(String keyword) {
+        List<Customer> customers=customerRepository.searchCustomer(keyword);
+        List<CustomerDTO> customerDTOS = customers.stream().map(cust -> dtoMapper.fromCustomer(cust)).collect(Collectors.toList());
+        return customerDTOS;
+    }
+
 
 }
--- src/main/java/org/sid/ebankingbackend/web/BankAccountRestApi.java
+++ src/main/java/org/sid/ebankingbackend/web/BankAccountRestApi.java
@@ -6,15 +6,12 @@ import org.sid.ebankingbackend.dtos.AccountOperationDTO;
 import org.sid.ebankingbackend.dtos.BankAccountDTO;
 import org.sid.ebankingbackend.exceptions.BankAccountNotFoundException;
 import org.sid.ebankingbackend.services.BankAccountService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-
+@CrossOrigin("*")
 public class BankAccountRestApi {
     private BankAccountService bankAccountService;
 
